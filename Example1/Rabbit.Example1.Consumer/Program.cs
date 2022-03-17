c0b0b6193af4430c6b1e9acee3bf24d78ecbc870
ali <ali.mahmoodi.heris@gmail.com>
2022-03-17 18:42:18 +0300

    init

---
+++ Example1/Rabbit.Example1.Consumer/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Immutable;
+using Rabbit.Common.Data.Weather;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Rabbit.Example1.Consumer
+{
+    internal sealed class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("\nEXAMPLE 1 : ONE-WAY MESSAGING : CONSUMER");
+
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = "172.16.0.207",
+                // UserName = "admin",
+                // Password = "password"
+            };
+
+            using var connection = connectionFactory.CreateConnection();
+
+            using var channel = connection.CreateModel();
+
+            var queue = channel.QueueDeclare(
+                queue: "example1_trades_queue",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: ImmutableDictionary<string, object>.Empty);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (sender, eventArgs) =>
+            {
+                var messageBody = eventArgs.Body.ToArray();
+                var forecast = Forecast.FromBytes(messageBody);
+                Console.WriteLine(forecast.ToJson());
+                
+                channel.BasicAck(eventArgs.DeliveryTag, multiple: false);
+            };
+
+            channel.BasicConsume(
+                queue: queue.QueueName,
+                autoAck: false,
+                consumer: consumer);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Example1/Rabbit.Example1.Producer/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading.Tasks;
+using RabbitMQ.Client;
+using System;
+using System.Drawing;
+using System.Collections.Immutable;
+using Rabbit.Common.Data.Weather;
+
+namespace Rabbit.Example1.Producer
+{
+    internal sealed class Program
+    {
+        private static async Task Main()
+        {
+            Console.WriteLine("\nEXAMPLE 1 : ONE-WAY MESSAGING : PRODUCER");
+
+            const string ExchangeName = "";
+            const string QueueName = "example1_trades_queue";
+
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = "172.16.0.207",
+                // UserName = "admin",
+                // Password = "password"
+            };
+
+            using var connection = connectionFactory.CreateConnection();
+
+            using var channel = connection.CreateModel();
+
+            var queue = channel.QueueDeclare(
+                queue: QueueName,
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: ImmutableDictionary<string, object>.Empty);
+
+            while (true)
+            {
+                var forecast =  Thermometer.Fake().Report();
+
+                channel.BasicPublish(
+                    exchange: ExchangeName,
+                    routingKey: QueueName,
+                    body: forecast.ToBytes()
+                );
+
+      Console.WriteLine(forecast.ToJson());
+
+                await Task.Delay(millisecondsDelay: 5000);
+            }
+        }
+    }
+}
